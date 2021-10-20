d24c0cd82d4e16abd7352a5578c9c966c358af32
m1cr050f75ux5 <rohanzealous@gmail.com>
2021-10-19 20:15:37 -0700

    uhhh

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -15,18 +15,21 @@ public class MyMain {
     // The largest number is 20
     // The smallest number is 10
     public static void main(String[] args) {
-        // YOUR CODE HERE FOR WARMUP
+        // Uncomment this code later!
+        System.out.println("Mathey.max tests");
+        System.out.println(Mathey.max(1, 2)); // 2
+        System.out.println(Mathey.max(2, 1)); // 2
         System.out.println(
-          randomTeen() + " " + randomTeen() + " " + randomTeen()
+            Mathey.max(2.0, 3.0, 4.0, 5.0)
+            + "\nMathey.randomInteger test " + Mathey.randomInteger(1,3) + " " + Mathey.randomInteger(3)
+            + "\nMathey.pow test " + Mathey.pow(3,4) + " " + Mathey.pow(4, 10)
+            + "\nMathey.abs test " + Mathey.abs(-9) + " " + Mathey.abs(-7676786)
+            + "\nMathey.round test " + Mathey.round(9.87) + " " + Mathey.round(99.5)
+            + "\nMathey.floor test " + Mathey.floor(2.99)  + " " + Mathey.floor(10.13)
+            + "\nMathey.ceil test " + Mathey.ceil(2.9) + " " + Mathey.ceil(1.01)
+//            + "\npythagoras theorem " + Mathey.sqrt(Mathey.pow(5,2) + Mathey.pow(12,2))
+//            Math.pow()
         );
-
-
-
-
-         // Uncomment this code later!
-         System.out.println("Mathey.max tests");
-         System.out.println(Mathey.max(1, 2)); // 2
-         System.out.println(Mathey.max(2, 1)); // 2
     }
 
 }
